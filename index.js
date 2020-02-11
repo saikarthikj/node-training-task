9110d5b869464f5800b65d3b4e51a016053f44ff
saikarthik <saikarthik@techjini.com>
2020-02-12 01:27:28 +0530

    updated code

--- index.js
+++ index.js
@@ -4,20 +4,50 @@ const args = process.argv.slice(2);
 const rl = readline.createInterface({
     input: process.stdin
 });
+const sortAsc = (result, item) => {
+    item = +item;
+    const length = result.length;
+    if (!length || (length && item >= result[length -1])) {
+        result.push(item);
+    } else if (item <= result[0]){
+        result.unshift(item);
+    } else {
+        const middle = Math.floor(length / 2);
+        const part1 = result.slice(0, middle);
+        const part2 = result.slice(middle, length);
+        if (item <= part1[part1.length - 1] || item <= part2[0]) {
+            result = sortAsc(part1, item).concat(part2);
+        } else if (item >= part2[0]) {
+            result = part1.concat(sortAsc(part2, item));
+        }
+    }
+    return result;
+}
+const sortDesc = (result, item) => {
+    item = +item;
+    const length = result.length;
+    if (!length || (length && item <= result[length -1])) {
+        result.push(item);
+    } else if (item >= result[0]){
+        result.unshift(item);
+    } else {
+        const middle = Math.floor(length / 2);
+        const part1 = result.slice(0, middle);
+        const part2 = result.slice(middle, length);
+        if (item <= part1[part1.length - 1] || item <= part2[0]) {
+            result = part1.concat(sortDesc(part2, item));
+        } else if (item >= part2[0]) {
+            result = sortDesc(part1, item).concat(part2);
+        }
+    }
+    return result;
+}
 rl.on('line', (data) => {
     let mode = 'asc';
     if (args.length) {
         const flag = args.find(item =>  item.indexOf('mode') >= 0);
         mode = (flag) ?  flag.split('=')[1] : 'asc'    
     }
-    const array = data.replace(/ /g, ',').split(',').sort((a, b) =>{   
-        if (+a > +b) {
-             return mode === 'asc' ? 1 : -1;
-        } else if (+a < +b) {
-            return mode === 'asc' ? -1 : 1;
-        }else {
-            return 0;
-        }
-    });
+    const array = data.replace(/ /g, ',').split(',').reduce(mode === 'asc'? sortAsc: sortDesc, []);
     console.log(`The ${mode} is ${array}`);
 });
--- os-data.js
+++ os-data.js
@@ -1,4 +1,4 @@
-var os = require('os'),
+const os = require('os'),
 cpuCount = os.cpus().length;
 console.log(`Node version is ${process.version}`);
 console.log(`No of cores in CPU is  ${cpuCount}`);
