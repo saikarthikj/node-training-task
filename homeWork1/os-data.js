eafd07e3638f9c5e601e51f5387bca68425da915
saikarthik <saikarthik@techjini.com>
2020-03-10 23:13:38 +0530

    review comments fix

--- homeWork1/os-data.js
+++ homeWork1/os-data.js
@@ -1,4 +1,4 @@
-const os = require('os'),
-cpuCount = os.cpus().length;
+const os = require('os');
+const cpuCount = os.cpus().length;
 console.log(`Node version is ${process.version}`);
 console.log(`No of cores in CPU is  ${cpuCount}`);
